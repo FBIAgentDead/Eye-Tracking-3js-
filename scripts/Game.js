83dd1f08b65d0785a5732d8deb1b54eb23cde43b
Dimitri van Manen <vmanen68@gmail.com>
2019-05-13 09:38:35 +0200

    Visual studio did a oopsie

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,11 +1,13 @@
 
 let camera;
-
+let width = window.innerWidth;
+let height = window.innerHeight;
 function Setup() {
     console.log("oof");
-
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 10);
+    // camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 10);
+    camera = new THREE.OrthographicCamera(width/-2, width/2, height/2, height/-2, 1, 1000);
     console.log(camera);
+
 }
 
 Setup();
