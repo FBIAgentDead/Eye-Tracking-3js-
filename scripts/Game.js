6571d5da1d0f9333db568ebb5e2efdd11351639a
jochemlam <30724588+jochemlam@users.noreply.github.com>
2019-05-14 14:58:08 +0200

    first small little steps for the project

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,13 +1,57 @@
+var controller;
+var hand;
+var cube
 
-let camera;
-let width = window.innerWidth;
-let height = window.innerHeight;
-function Setup() {
-    console.log("oof");
-    // camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 10);
-    camera = new THREE.OrthographicCamera(width/-2, width/2, height/2, height/-2, 1, 1000);
-    console.log(camera);
+function setup() {
+    //leap setup
+    controller = new Leap.Controller()
+    controller.connect();
+    controller.setBackground(true);
+    controller.on('frame', update);
+    controller.on('connect', onConnect);
+    controller.on('disconnect', onDisconnect);
+    controller.on('streamingStarted', onDeviceConnected);
+    controller.on('streamingStopped', onDeviceDisconnected);
 
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    update();
 }
 
-Setup();
+//when the leap is connected
+function onConnect() {
+    console.log("Connection succesfully established.");
+}
+//when the leap is disconnected
+function onDisconnect() {
+    console.log("Connection timed out.");
+}
+//when the cable is connected
+function onDeviceConnected() {
+    console.log("Device connected.");
+}
+//when the cable is disconnected
+function onDeviceDisconnected() {
+    console.log("Device has been disconnected.");
+}
+//this is the update that gets called every frame
+function update() {
+    
+    //code here
+    requestAnimationFrame(update);
+}
+
+Leap.loop(function (frame) {
+    hand = frame.hands[0];
+    if(hand != null){
+        cube.rotation.x += hand.palmNormal[0] / 20;
+        cube.rotation.y += hand.palmNormal[1] / 20;
+        cube.rotation.z += hand.palmNormal[2] / 20;
+    }
+    // insert stupid code joke here
+});
+
+setup();
